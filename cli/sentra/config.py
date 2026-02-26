74fc04250d0040fd704fa4c7db35657b260ed842
johnathanbryce <johnathanbryce@gmail.com>
2026-02-25 16:24:38 -0800

    update cli config

--- cli/sentra/config.py
+++ cli/sentra/config.py
@@ -2,8 +2,11 @@ import os
 
 ENVIRONMENT = os.environ.get("ENVIRONMENT", "prod")
 
-API_BASE_URL = ""
-if ENVIRONMENT == "dev":
-    API_BASE_URL = "http://localhost:8000"
-else:
-    API_BASE_URL = ""  # TODO: PROD
+
+API_BASE_URL = (
+    "http://localhost:8000" if ENVIRONMENT == "dev" else ""
+)  # TODO: update prod api
+
+API_VERSION_PREFIX = "/api/v1"
+
+KEYCHAIN_SERVICE = "sentrafi"
